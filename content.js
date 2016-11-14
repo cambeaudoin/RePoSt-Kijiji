48a2c71e619a4035fe6d1a85c7d2d55552216f03
Cam Beaudoin <camilebeaudoin@gmail.com>
2016-11-14 13:40:34 -0500

    Attempting to manipulate data

--- content.js
+++ content.js
@@ -8,35 +8,47 @@ var copyOldAd = function (id) {
     var request = {};
     request.url = "https://www.kijiji.ca/v-view-details.html?requestSource=b&adId=" + id;
 
-    chrome.runtime.sendMessage({oldAd: request.url}, function(response) {
-        console.log(response.getElementById("BigBoxSponsor"));
-        var formattedResponse = new DOMParser().parseFromString(response, "text/xml");
-        console.log(formattedResponse);
+    chrome.runtime.sendMessage({ oldAd: request.url }, function (response) {
+        var el = document.createElement( 'html' );
+        el.innerHTML = response;
+
+        var itemprops = el.getElementsByTagName( 'span' );
+        Array.prototype.forEach.call(itemprops, function(item) {
+            if (item.hasAttribute('itemprop')) {
+                
+                console.log(item.getAttribute('itemprop'));
+                console.log(item);
+            }
+        })
+        console.log();
+
+        var newAd = {};
+        newAd.title = el.getElementsByTagName( 'h1' );
+
     });
 };
 
+setTimeout(function () {
+    var links = document.getElementsByTagName("a");
 
+    Array.prototype.forEach.call(links, function (link, index) {
 
+        if (link.id && link.id.indexOf("delete-ad-") === 0) {
 
+            var adId = link.id.slice(10),
+                repost = document.createElement("a");
 
-var links = document.getElementsByTagName("a");
+            repost.className = link.className;
+            repost.href = "";
+            repost.appendChild(document.createTextNode("RePoSt Ad"));
+            repost.id = adId;
 
-Array.prototype.forEach.call(links, function (link, index) {
+            link.parentNode.appendChild(repost);
+            repost.addEventListener("click", function () {
+                copyOldAd(repost.id);
+                // deleteOldAd();
+            }, false);
+        }
+    })
 
-    if (link.id && link.id.indexOf("delete-ad-") === 0) {
-
-        var adId = link.id.slice(10),
-            repost = document.createElement("a");
-
-        repost.className = link.className;
-        repost.href = "";
-        repost.appendChild(document.createTextNode("RePoSt Ad"));
-        repost.id = adId;
-
-        link.parentNode.appendChild(repost);
-        repost.addEventListener("click", function () {
-            copyOldAd(repost.id);
-            // deleteOldAd();
-        }, false);
-    }
-})
+}, 1000) 
